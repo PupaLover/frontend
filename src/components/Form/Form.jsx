616235dc52ae803f92a4c4b0d60521a2a7524e09
PupaLover <lukicevgeorgij@gmail.com>
2024-11-06 16:27:09 +0300

    1

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -19,9 +19,9 @@ const ProductList = () => {
     })
 
     useEffect(() => {
-        tg.onEvent('mainButtonClicked', callback)
+        tg.onEvent('mainButtonClicked', onSendData)
         return() => {
-            tg.offEvent('mainButtonClicked', callback)
+            tg.offEvent('mainButtonClicked', onSendData)
         }
     },[])
 
